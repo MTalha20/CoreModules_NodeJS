a3ee3117a1268ceac156849b7477b114a8e03cf5
MTalha20 <talha.wm15@gmail.com>
2023-01-06 11:58:39 +0500

    WorkComplete

---
+++ asyncFileSys/index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+// ASYNC FILE SYSTEM
+
+// fs.writeFile("readFile.txt", "Muhammad Talha ", (err)=>{console.log("File Created");});
+
+// fs.appendFile("readFile.txt", "\nStuding NodeJS by technical thapa", (err)=>{console.log("File appends");});
+
+// const data =  fs.readFile("readFile.txt", "utf-8", (err,data)=>{console.log(data);});
+
+// fs.rename("readFile.txt", "read.txt", (err)=>{console.log("File  Renamed");});
+
+fs.readFile("read.txt", "utf-8", (err,data) => {
+    console.log(data);
+});
+
+console.log("after data");
---
+++ chalengeTask/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+// CHALLENGE TASK <==> CRUD Operations
+
+// fs.mkdirSync('talha'); 
+
+// fs.writeFileSync('thapa/bio.txt', "My First Task");
+
+// fs.appendFileSync('talha/bio.txt', "\nBy thapa tecnical")
+
+// const data  = fs.readFileSync('talha/bio.txt', 'utf-8');
+// console.log(data);
+
+// fs.renameSync('talha/bio.txt', 'talha/mybio.txt');
+
+// fs.unlinkSync('talha/mybio.txt');
+ 
+// fs.rmdir('talha')
+
+// Task Completed <<<---->>
